16fec7acafcbfb252bf39deedec3f454c0507659
Aizal Manan <coisox@gmail.com>
2017-12-29 23:34:28 +0800

    Function to remove previous precache PWA

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,5 @@
 //This is the service worker with the Cache-first network
-
-var CACHE = 'pwabuilder-precache';
+var CACHE = 'precache-20171229b';
 var precacheFiles = [
 	'index.html',
 	'./', // Alias for index.html
@@ -25,56 +24,63 @@ var precacheFiles = [
 self.addEventListener('install', function(evt) {
 	console.log('The service worker is being installed.');
 	evt.waitUntil(precache().then(function() {
-		console.log('[ServiceWorker] Skip waiting on install');
+			console.log('[ServiceWorker] Skip waiting on install');
 			return self.skipWaiting();
-
-	})
+		})
 	);
 });
 
 
 //allow sw to control of current page
 self.addEventListener('activate', function(event) {
-console.log('[ServiceWorker] Claiming clients for current page');
-      return self.clients.claim();
+	var cacheWhitelist = ['precache-20171229b'];
 
+	event.waitUntil(
+		caches.keys().then(function(keyList) {
+			return Promise.all(keyList.map(function(key) {
+				if (cacheWhitelist.indexOf(key) === -1) {
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
 });
 
 self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.'+ evt.request.url);
-  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
-  evt.waitUntil(update(evt.request));
+	console.log('The service worker is serving the asset.'+ evt.request.url);
+	evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+	evt.waitUntil(update(evt.request));
 });
 
 
 function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(precacheFiles);
-  });
+	return caches.open(CACHE).then(function (cache) {
+		return cache.addAll(precacheFiles);
+	});
 }
 
 
 function fromCache(request) {
-  //we pull files from the cache first thing so we can show them fast
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
+	//we pull files from the cache first thing so we can show them fast
+	return caches.open(CACHE).then(function (cache) {
+		return cache.match(request).then(function (matching) {
+			return matching || Promise.reject('no-match');
+		});
+	});
 }
 
 
 function update(request) {
-  //this is where we call the server to get the newest version of the 
-  //file to use the next time we show view
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
+	//this is where we call the server to get the newest version of the 
+	//file to use the next time we show view
+	return caches.open(CACHE).then(function (cache) {
+		return fetch(request).then(function (response) {
+			return cache.put(request, response);
+		});
+	});
 }
 
 function fromServer(request){
-  //this is the fallback if it is not in the cahche to go to the server and get it
-return fetch(request).then(function(response){ return response})
+	//this is the fallback if it is not in the cahche to go to the server and get it
+	return fetch(request).then(function(response){ return response})
 }
