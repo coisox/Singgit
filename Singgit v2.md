2c50ecbb6928caa35ecf01e006845cb7881a3fb3
Aizal Manan <coisox@gmail.com>
2020-06-08 10:48:50 +0800

    v20200608

---
+++ Singgit v2.md
@@ -0,0 +1,5 @@
+# TODO
+
+1. Use Bootstrap 5
+2. Use virtual scroll list (https://github.com/tangbc/vue-virtual-scroll-list)
+3. Use Vue CLI
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 app = new Vue({
 	el: '#app',
 	data: {
-		version: 'v20200517',
+		version: 'v20200608',
 		progress: false,
 		dbx: new Dropbox.Dropbox({accessToken: 'gLb9sbW8xDgAAAAAAAADyIxcjH6QBxbYI7o6qWl31VQweZV2b1U7MEcrq9X-hh6c'}),
 		cloud: {
